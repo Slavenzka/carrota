15b824e46833c1c916541fd2a8d646d8c1658b46
Ivan <i.romanov@protonmail.com>
2020-09-15 19:12:00 +0300

    Updates

---
+++ precache-manifest.a14cfc18bed85d6ac754aae89234064e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4cf5e7aa108cdf489426b2f5d744d5",
+    "url": "/carrota/index.html"
+  },
+  {
+    "revision": "14fb334bc6ebcb7c237f",
+    "url": "/carrota/static/css/2.769dd305.chunk.css"
+  },
+  {
+    "revision": "a203bb2dc945483ef94d",
+    "url": "/carrota/static/css/main.30ca9774.chunk.css"
+  },
+  {
+    "revision": "14fb334bc6ebcb7c237f",
+    "url": "/carrota/static/js/2.a73147c6.chunk.js"
+  },
+  {
+    "revision": "5b099948b207b77f6fa04ff33fa98702",
+    "url": "/carrota/static/js/2.a73147c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a203bb2dc945483ef94d",
+    "url": "/carrota/static/js/main.98d89184.chunk.js"
+  },
+  {
+    "revision": "74bdc9542dd344e8c42e",
+    "url": "/carrota/static/js/runtime-main.cd3122b7.js"
+  },
+  {
+    "revision": "35dbae879b69268bfb5834c644594829",
+    "url": "/carrota/static/media/Roboto-Light.35dbae87.woff2"
+  },
+  {
+    "revision": "377210850035494700cef3e577fba77a",
+    "url": "/carrota/static/media/Roboto-Light.37721085.woff"
+  },
+  {
+    "revision": "84e014ccc5058c7c7f2117ead4336dc1",
+    "url": "/carrota/static/media/Roboto-Regular.84e014cc.woff"
+  },
+  {
+    "revision": "b18854c16b385247971d11793b0d2e1c",
+    "url": "/carrota/static/media/Roboto-Regular.b18854c1.woff2"
+  },
+  {
+    "revision": "b99320da5f1acc3045b58d2a07d87ad5",
+    "url": "/carrota/static/media/SFProDisplay-Heavy.b99320da.woff"
+  },
+  {
+    "revision": "fee26660875bec06ce508e96057f6efa",
+    "url": "/carrota/static/media/SFProDisplay-Heavy.fee26660.woff2"
+  },
+  {
+    "revision": "0229ea633558c4c9c1fbc1c92e5a0de3",
+    "url": "/carrota/static/media/SFProDisplay-Light.0229ea63.woff2"
+  },
+  {
+    "revision": "ee7481b4e87fdad06d71aeac1d173821",
+    "url": "/carrota/static/media/SFProDisplay-Light.ee7481b4.woff"
+  },
+  {
+    "revision": "52ea0d7008516c101595f81d37225889",
+    "url": "/carrota/static/media/SFProDisplay-Medium.52ea0d70.woff2"
+  },
+  {
+    "revision": "9f7769f51fe242081d7fec9dff7bea58",
+    "url": "/carrota/static/media/SFProDisplay-Medium.9f7769f5.woff"
+  },
+  {
+    "revision": "89a0dda9a4e0fcbf9d90f507feb77486",
+    "url": "/carrota/static/media/SFProDisplay-Regular.89a0dda9.woff"
+  },
+  {
+    "revision": "abe60269bd99b7f36cd026bd02af92a6",
+    "url": "/carrota/static/media/SFProDisplay-Regular.abe60269.woff2"
+  }
+]);
